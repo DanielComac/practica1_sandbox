4d04b5421e32c7c97011f692fe466281ce272455
DanielComac <115488114+DanielComac@users.noreply.github.com>
2023-01-13 14:44:03 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Mi primera practica con Sandbox</h1>
+<h2>Compean Macias Gerardo Daniel</h2>
+<div>
+  Bienvenido a mi repositorio de GitHub
+  <a href="https://github.com/DanielComac/contador.git" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
